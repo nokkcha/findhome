4de6bd29949a3011f7bfac20857f7595008f5770
imrutel <imrutel@gmail.com>
2021-05-06 19:15:39 +0900

    컨트롤러 수정

--- src/main/java/com/itwillbs/controller/MemberController.java
+++ src/main/java/com/itwillbs/controller/MemberController.java
@@ -27,34 +27,11 @@ public class MemberController {
 		return "join_choice";
 	}
 	
-	@RequestMapping(value = "/member/update", method = RequestMethod.GET)
-	public String update(HttpSession session, Model model) {
-		// String id = 세션값 가져오기
-		String id=(String)session.getAttribute("id");
-		// MemberBean mb = 세션에 해당하는 정보 조회 getMember(id)   MemberBean 리턴
-		MemberBean mb=memberService.getMember(id);
-		// model "mb",mb
-		model.addAttribute("mb",mb);
-		
-		//  /WEB-INF/views/member/updateForm.jsp
-		return "member/updateForm";
+	@RequestMapping(value = "/join",method = RequestMethod.GET )
+	public String join() {
+		return "join";
 	}
 	
-	@RequestMapping(value = "/member/updatePro", method = RequestMethod.POST)
-	public String updatePro(MemberBean mb,  Model model) {
-		
-		MemberBean mb2=memberService.userCheck(mb);
-		if(mb2!=null) {
-			// 수정
-			memberService.updateMember(mb);
-			return "redirect:/member/main";
-		}else {
-			// 입력하신 정보가 틀립니다. 
-			model.addAttribute("msg","입력하신 정보가 틀립니다.");
-		//  /WEB-INF/views/member/msg.jsp
-			return "member/msg";
-		}
-	}
 	
 	@RequestMapping(value = "/joinPro", method = RequestMethod.POST)
 	public String joinPro(MemberBean mb) {
@@ -64,16 +41,6 @@ public class MemberController {
 		return "redirect:/";
 	}
 	
-	@RequestMapping(value = "/member/list", method = RequestMethod.GET)
-	public String list( Model model) {
-		// List<MemberBean> mbList = 회원전체 정보 조회 getMemberList()   List<MemberBean> 리턴
-		List<MemberBean> mbList=memberService.getMemberList();
-		// model "mbList",mbList
-		model.addAttribute("mbList",mbList);
-		
-		//  /WEB-INF/views/member/list.jsp
-		return "member/list";
-	}
 	
 }
 
