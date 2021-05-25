5f51fd3f5945952d40bc8893291ca61f1134c5d7
imrutel <imrutel@gmail.com>
2021-05-25 12:25:14 +0900

    섬네일 생성 크기 수정 (100->200)

--- src/main/java/com/itwillbs/upload/UploadFileUtils.java
+++ src/main/java/com/itwillbs/upload/UploadFileUtils.java
@@ -80,8 +80,8 @@ public class UploadFileUtils {
 	    private static String makeThumbnail(String uploadPath, String path, String fileName) throws Exception {
 	        // 이미지를 읽기 위한 버퍼
 	        BufferedImage sourceImg = ImageIO.read(new File(uploadPath + path, fileName));
-	        // 100픽셀 단위의 썸네일 생성
-	        BufferedImage destImg = Scalr.resize(sourceImg, Scalr.Method.AUTOMATIC, Scalr.Mode.FIT_TO_HEIGHT, 100);
+	        // *픽셀 단위의 썸네일 생성
+	        BufferedImage destImg = Scalr.resize(sourceImg, Scalr.Method.AUTOMATIC, Scalr.Mode.FIT_TO_HEIGHT, 200);
 	        // 썸네일의 이름을 생성(원본파일명에 's_'를 붙임)
 	        String thumbnailName = uploadPath + path + File.separator + "s_" + fileName;
 	        File newFile = new File(thumbnailName);
